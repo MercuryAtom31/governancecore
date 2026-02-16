fe47422be65503fd57bed6a859daf90f08791b93
Hichem <hichembenzair@gmail.com>
2026-02-15 23:46:01 -0500

    first commit

---
+++ governancecore/src/main/java/com/benzair/governancecore/GovernancecoreApplication.java
@@ -0,0 +1,13 @@
+package com.benzair.governancecore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovernancecoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovernancecoreApplication.class, args);
+	}
+
+}
---
+++ governancecore/src/test/java/com/benzair/governancecore/GovernancecoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benzair.governancecore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovernancecoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
