3b429b5ee3d932dcb47e5548a3e69396b88461be
Hichem <hichembenzair@gmail.com>
2026-02-16 00:39:00 -0500

    Finished coding the entity class.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
@@ -1,25 +1,34 @@
 package com.benzair.governancecore.assetsubdomain.datalayer;
 import jakarta.persistence.*;
-import lombok.Getter;
 import lombok.*;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
-@Table(name = "assets")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+@Table(name = "assets") // Table name in the database
+@Getter // Lombok annotation to generate getters
+@Setter // Lombok annotation to generate setters
+@NoArgsConstructor // Lombok annotation to generate a no-args constructor
+@AllArgsConstructor // Lombok annotation to generate an all-args constructor
+@Builder // Lombok annotation to implement the builder pattern
 public class Asset {
    @Id
-   public String id;
-   public String name;
-   public String owner;
-   public String classification;
-   public LocalDateTime createdAt;
-   public LocalDateTime updatedAt;
+   @GeneratedValue(strategy = GenerationType.UUID)
+   private UUID id;
 
+   @Column (nullable = false)
+   private String name;
 
+   @Column (nullable = false)
+   private String owner;
+
+   @Column (nullable = false)
+   private String classification;
+
+   @Column (nullable = false)
+   private LocalDateTime createdAt;
+
+   @Column (nullable = false)
+   private LocalDateTime updatedAt;
 
 }
