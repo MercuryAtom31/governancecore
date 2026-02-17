613b7f42f41db487902d8b303809dd9b1ca45474
Hichem <hichembenzair@gmail.com>
2026-02-16 23:41:21 -0500

    Added two methods so timestamps are auto-managed by JPA: @PrePersist: initializes createdAt and updatedAt @PreUpdate: refreshes updatedAt So service/controller code doesn’t need to set timestamps manually.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
@@ -1,8 +1,20 @@
 package com.benzair.governancecore.assetsubdomain.datalayer;
-import jakarta.persistence.*;
-import lombok.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /*
 Entity means:
@@ -32,10 +44,23 @@ public class Asset {
    @Column (nullable = false)
    private String classification;
 
-   @Column (nullable = false)
+   @Column (nullable = false, updatable = false) // This column cannot be updated after creation
    private LocalDateTime createdAt;
 
    @Column (nullable = false)
    private LocalDateTime updatedAt;
+
+   // @PrePersist runs only when that row is inserted the first time.
+   @PrePersist // JPA annotation to specify that this method should be called before the entity is persisted (saved for the first time)
+   private void prePersist() {
+      LocalDateTime now = LocalDateTime.now(); // Get the current time
+      createdAt = now; // Set createdAt to the current time when the entity is first persisted
+      updatedAt = now; // Set updatedAt to the current time when the entity is first persisted
+   }
+
+   @PreUpdate // JPA annotation to specify that this method should be called before the entity is updated
+   private void preUpdate() {
+      updatedAt = LocalDateTime.now(); // Update updatedAt to the current time whenever the entity is updated
+   }
 
 }
