cf162c104234189c390eb45df37d59d94bca9221
Hichem <hichembenzair@gmail.com>
2026-02-16 23:15:44 -0500

    Added explanation comments.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/Asset.java
@@ -4,18 +4,25 @@ import lombok.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
+/*
+Entity means:
+a Java class that represents a table in a database. 
+*/
+@Entity // JPA (Java Persistence API) annotation to mark this class as a database entity
 @Table(name = "assets") // Table name in the database
 @Getter // Lombok annotation to generate getters
 @Setter // Lombok annotation to generate setters
 @NoArgsConstructor // Lombok annotation to generate a no-args constructor
 @AllArgsConstructor // Lombok annotation to generate an all-args constructor
 @Builder // Lombok annotation to implement the builder pattern
+
 public class Asset {
-   @Id
-   @GeneratedValue(strategy = GenerationType.UUID)
+
+   @Id // JPA (Java Persistence API) annotation to mark this field as the primary key
+   @GeneratedValue(strategy = GenerationType.UUID) // JPA annotation to specify that the ID should be generated as a UUID
    private UUID id;
 
+   // JPA annotation to specify that this column cannot be null
    @Column (nullable = false)
    private String name;
 
