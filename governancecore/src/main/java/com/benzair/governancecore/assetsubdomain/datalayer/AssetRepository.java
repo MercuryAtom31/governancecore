bfefdbc0cfe5d82ebc67c55e56331b51810a83a9
Hichem <hichembenzair@gmail.com>
2026-02-19 21:33:39 -0500

    Created the repository interface.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/AssetRepository.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datalayer/AssetRepository.java
@@ -1,5 +1,13 @@
 package com.benzair.governancecore.assetsubdomain.datalayer;
 
-public class AssetRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+import java.util.Optional;
+
+public interface AssetRepository extends  JpaRepository<Asset, UUID> {
+    // JpaRepository provides basic CRUD operations and pagination for Asset entity.
+
+    Optional<Asset> findByAssetIdentifier_AssetId(String assetId);
+    // Custom query method to find an Asset by its business ID (external uuid) using the embedded AssetIdentifier.
     
 }
