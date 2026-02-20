f220f49ced4272c89879a30cf225c56ab918b5aa
Hichem <hichembenzair@gmail.com>
2026-02-19 22:58:57 -0500

    Created a response mapper interface that converts DB entity shape into API response shape.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datamapperlayer/AssetResponseMapper.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datamapperlayer/AssetResponseMapper.java
@@ -1,5 +1,17 @@
 package com.benzair.governancecore.assetsubdomain.datamapperlayer;
 
+import java.util.List;
+import com.benzair.governancecore.assetsubdomain.presentationlayer.AssetResponseModel;
+import com.benzair.governancecore.assetsubdomain.datalayer.Asset;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
 public interface AssetResponseMapper {
     
+
+    @Mapping(source = "assetIdentifier.assetId", target = "assetId")
+    AssetResponseModel entityToResponseModel(Asset asset);
+
+    List<AssetResponseModel> entityToResponseModelList(List<Asset> assets);
 }
