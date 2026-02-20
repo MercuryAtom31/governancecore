d8afd436e3c298495e6ba9eb9c59d8e956feece3
Hichem <hichembenzair@gmail.com>
2026-02-19 23:57:08 -0500

    Added explanatory comments about the role of a request mapper interface.

--- governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datamapperlayer/AssetRequestMapper.java
+++ governancecore/src/main/java/com/benzair/governancecore/assetsubdomain/datamapperlayer/AssetRequestMapper.java
@@ -10,9 +10,16 @@ AssetRequestMapper
 Responsibility:
 Convert incoming request into an Asset entity for service save/update flow.
 */
+
+/*
+This is a MapStruct mapper interface.
+It tells MapStruct:
+"Generate code that converts an AssetRequestModel into an Asset entity."
+*/
 @Mapper(componentModel = "spring")
 public interface AssetRequestMapper {
     
+    // This is saying: "Even if the request contains a field named id, DO NOT copy it into the entity."
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "assetIdentifier", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
