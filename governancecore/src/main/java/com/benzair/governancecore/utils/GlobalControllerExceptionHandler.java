f2b3bed9faf92a1413a988c2819df9166202dd16
Hichem <hichembenzair@gmail.com>
2026-02-25 22:44:42 -0500

    Created two exception handler methods that tell Spring that they should be called when their respective errors are thrown.

--- governancecore/src/main/java/com/benzair/governancecore/utils/GlobalControllerExceptionHandler.java
+++ governancecore/src/main/java/com/benzair/governancecore/utils/GlobalControllerExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.benzair.governancecore.utils;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.benzair.governancecore.utils.exceptions.InvalidInputException;
+import com.benzair.governancecore.utils.exceptions.ResourceNotFoundException;
+
 /*
 GlobalControllerExceptionHandler represents:
 "If any of those exceptions are thrown, I decide what response to return."
@@ -8,8 +16,19 @@ It does NOT throw errors.
 
 It catches them.
 */
+@RestControllerAdvice // This annotation tells Spring that this class will handle exceptions globally for all controllers.
 public class GlobalControllerExceptionHandler {
     
+    @ExceptionHandler(InvalidInputException.class) // This annotation tells Spring that this method should be called when an InvalidInputException is thrown.
+    public ResponseEntity<ApiError> handleInvalid(InvalidInputException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError(exception.getMessage()));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiError> handleNotFound(ResourceNotFoundException exception)
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                         .body(new ApiError(exception.getMessage()));
 }
 
 /*
