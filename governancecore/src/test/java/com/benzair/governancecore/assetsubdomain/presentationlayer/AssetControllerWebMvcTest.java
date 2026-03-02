4da05d7351a93567d5173946d8708154a3eac63a
Hichem <hichembenzair@gmail.com>
2026-03-01 22:39:51 -0500

    Added controller test.

---
+++ governancecore/src/test/java/com/benzair/governancecore/assetsubdomain/presentationlayer/AssetControllerWebMvcTest.java
@@ -0,0 +1,62 @@
+package com.benzair.governancecore.assetsubdomain.presentationlayer;
+
+import com.benzair.governancecore.assetsubdomain.businesslayer.AssetService;
+import com.benzair.governancecore.assetsubdomain.datalayer.AssetType;
+import com.benzair.governancecore.assetsubdomain.datalayer.DataClassification;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(AssetController.class)
+public class AssetControllerWebMvcTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private AssetService assetService;
+
+    @Test
+    void getAllAssets_shouldReturnAllAssets() throws Exception {
+
+        // ARRANGE
+        List<AssetResponseModel> assets = List.of(
+                AssetResponseModel.builder()
+                        .name("Server 1")
+                        .owner("IT department")
+                        .assetType(AssetType.SERVER)
+                        .classification(DataClassification.CONFIDENTIAL)
+                        .description("Handles customer data")
+                        .build()
+        );
+
+        when(assetService.getAllAssets()).thenReturn(assets);
+
+        // ACT
+        mockMvc.perform(get("/api/v1/assets")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].name").value("Server 1"))
+                .andExpect(jsonPath("$[0].owner").value("IT department"));
+
+        // ASSERT
+        verify(assetService).getAllAssets();
+        verifyNoMoreInteractions(assetService);
+    }
+}
