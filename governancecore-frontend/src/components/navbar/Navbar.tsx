f99b86370265de92cfcd4811181adb5a2dc8612f
Hichem <hichembenzair@gmail.com>
2026-03-10 23:48:31 -0400

    Changed location of the navbar.

---
+++ governancecore-frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+export default function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    return(
+        <div>
+            <nav className="navbar-container">
+                
+            </nav>
+        </div>
+    );
+}
