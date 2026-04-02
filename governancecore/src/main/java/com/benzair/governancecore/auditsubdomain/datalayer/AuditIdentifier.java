af39e26be305d07a7d156b6ef1f99fd104039374
Hichem <hichembenzair@gmail.com>
2026-04-01 23:02:55 -0400

    Created the audit identifier which will be the audit public ID.

--- governancecore/src/main/java/com/benzair/governancecore/auditsubdomain/datalayer/AuditIdentifier.java
+++ governancecore/src/main/java/com/benzair/governancecore/auditsubdomain/datalayer/AuditIdentifier.java
@@ -1,5 +1,20 @@
 package com.benzair.governancecore.auditsubdomain.datalayer;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.UUID;
+
+@Embeddable
+@Data
+@AllArgsConstructor
 public class AuditIdentifier {
     
+    @Column(name = "audit_id", nullable = false, unique = true)
+    private UUID auditId;
+
+    public AuditIdentifier() {
+        this.auditId = java.util.UUID.randomUUID();
+    }
 }
