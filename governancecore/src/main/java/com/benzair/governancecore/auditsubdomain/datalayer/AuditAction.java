d9612d34bb720401da00e059be27b5f57140418d
Hichem <hichembenzair@gmail.com>
2026-04-15 22:46:03 -0400

    Created the audit actions representing the types of operations that can be audited.

---
+++ governancecore/src/main/java/com/benzair/governancecore/auditsubdomain/datalayer/AuditAction.java
@@ -0,0 +1,10 @@
+package com.benzair.governancecore.auditsubdomain.datalayer;
+
+public enum AuditAction {
+    ASSET_CREATED,
+    ASSET_UPDATED,
+    ASSET_DELETED,
+    ACCESS_DENIED
+}
+
+// These actions represent the types of operations that can be audited.
