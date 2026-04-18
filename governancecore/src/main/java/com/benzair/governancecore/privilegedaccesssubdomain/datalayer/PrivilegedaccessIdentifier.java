6760088e18d5e0fc75ac2f87355ad2f6014dbdfd
Hichem <hichembenzair@gmail.com>
2026-04-17 21:58:40 -0400

    Created the privileged access identifier.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessIdentifier.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessIdentifier.java
@@ -1,8 +1,20 @@
 ﻿package com.benzair.governancecore.privilegedaccesssubdomain.datalayer;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.util.UUID;
+
 @Embeddable
 @Data
 @AllArgsConstructor
 public class PrivilegedAccessIdentifier {
     
+    @Column(name = "privileged_access_id", nullable = false, unique = true)
+    private String privilegedAccessId;
+
+    public PrivilegedAccessIdentifier() {
+        this.privilegedAccessId = UUID.randomUUID();
+    }
 }
