225949fc8485623cbace3bd914862cfa27569001
Hichem <hichembenzair@gmail.com>
2026-04-17 21:52:22 -0400

    Added the necessary annotations.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/Privilegedaccess.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/Privilegedaccess.java
@@ -1,5 +1,15 @@
 ﻿package com.benzair.governancecore.privilegedaccesssubdomain.datalayer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "privileged_access")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PrivilegedAccess {
-    
+
 }
