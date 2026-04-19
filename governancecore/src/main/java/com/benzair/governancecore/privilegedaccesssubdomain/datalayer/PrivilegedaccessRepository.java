ab41149f5879a93521f532b2c9e6cfefac53e414
Hichem <hichembenzair@gmail.com>
2026-04-18 20:52:03 -0400

    Removed unnecessary import statement.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessRepository.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessRepository.java
@@ -3,7 +3,6 @@ package com.benzair.governancecore.privilegedaccesssubdomain.datalayer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
-import com.benzair.governancecore.assetsubdomain.datalayer.Asset;
 
 public interface PrivilegedAccessRepository extends JpaRepository<PrivilegedAccess, UUID> {
     
