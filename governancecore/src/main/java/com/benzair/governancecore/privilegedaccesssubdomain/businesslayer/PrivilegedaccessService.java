89bde8579137d3b6b811117396d7b297dd746420
Hichem <hichembenzair@gmail.com>
2026-04-19 12:30:48 -0400

    Added the interface service methods.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/businesslayer/PrivilegedaccessService.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/businesslayer/PrivilegedaccessService.java
@@ -1,5 +1,19 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.businesslayer;
 
+import com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer.PrivilegedAccessResponseModel;
+
+import java.util.List;
+import java.util.UUID;
+
 public interface PrivilegedAccessService {
     
+    List<PrivilegedAccessResponseModel> getAllPrivilegedAccesses();
+
+    PrivilegedAccessResponseModel getPrivilegedAccessByPrivilegedAccessId(UUID privilegedAccessId);
+
+    void deletePrivilegedAccessByPrivilegedAccessId(UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel addPrivilegedAccess(PrivilegedAccessResponseModel privilegedAccessRequestModel);
+
+    PrivilegedAccessResponseModel updatePrivilegedAccess(PrivilegedAccessResponseModel privilegedAccessRequestModel, UUID privilegedAccessId);
 }
