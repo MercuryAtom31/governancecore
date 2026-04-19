c4ee531f54f577af3d65a46b96a82c4fb686eb27
Hichem <hichembenzair@gmail.com>
2026-04-18 20:43:57 -0400

    Added missing import statement.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessRepository.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedaccessRepository.java
@@ -2,6 +2,7 @@ package com.benzair.governancecore.privilegedaccesssubdomain.datalayer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
 import com.benzair.governancecore.assetsubdomain.datalayer.Asset;
 
 public interface PrivilegedAccessRepository extends JpaRepository<PrivilegedAccess, UUID> {
