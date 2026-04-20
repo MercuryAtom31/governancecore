d0902ca3a3cb278d59f30b2558c05368ffa82451
Hichem <hichembenzair@gmail.com>
2026-04-19 20:39:22 -0400

    Created an access status enum.

---
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datalayer/PrivilegedAccessStatus.java
@@ -0,0 +1,10 @@
+package com.benzair.governancecore.privilegedaccesssubdomain.datalayer;
+
+public enum PrivilegedAccessStatus {
+    REQUESTED,
+    GRANTED,
+    IN_USE,
+    REFUSED,
+    EXPIRED,
+    REVOKED
+}
