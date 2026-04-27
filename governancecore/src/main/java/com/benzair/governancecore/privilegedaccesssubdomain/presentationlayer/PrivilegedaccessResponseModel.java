c36bfc16e645b275df13abc2a0302f15e562160b
Hichem <hichembenzair@gmail.com>
2026-04-26 23:23:23 -0400

    Created the privileged access response model.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/presentationlayer/PrivilegedaccessResponseModel.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/presentationlayer/PrivilegedaccessResponseModel.java
@@ -1,5 +1,34 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccessDuration;
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccessRole;
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccessStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PrivilegedAccessResponseModel {
-    
+
+    private UUID privilegedAccessId;
+    private String requesterName;
+    private String requesterCurrentRole;
+    private PrivilegedAccessRole role;
+    private PrivilegedAccessDuration durationMinutes;
+    private String justification;
+    private Instant requestedAt;
+    private PrivilegedAccessStatus status;
+    private String grantedByName;
+    private Instant grantedAt;
+    private Instant refusedAt;
+    private Instant expiresAt;
+    private Instant revokedAt;
 }
