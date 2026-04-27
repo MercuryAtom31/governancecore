5e17f4e0c13aa57a04a898f080a219f725886852
Hichem <hichembenzair@gmail.com>
2026-04-26 23:12:42 -0400

    Created the privileged access request model.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/presentationlayer/PrivilegedaccessRequestModel.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/presentationlayer/PrivilegedaccessRequestModel.java
@@ -1,5 +1,23 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer;
 
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccessDuration;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PrivilegedAccessRequestModel {
-    
+
+    @NotNull
+    private PrivilegedAccessDuration durationMinutes;
+
+    @NotBlank
+    private String justification;
 }
