e1b33dba13148d5fef5a3fa554a5a9f7f8ea8d6b
Hichem <hichembenzair@gmail.com>
2026-04-26 23:43:34 -0400

    Privileged access response mapper.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datamapperlayer/PrivilegedaccessResponseMapper.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datamapperlayer/PrivilegedaccessResponseMapper.java
@@ -1,5 +1,18 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.datamapperlayer;
 
-public class PrivilegedAccessResponseMapper {
-    
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccess;
+import com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer.PrivilegedAccessResponseModel;
+
+@Mapper(componentModel = "spring")
+public interface PrivilegedAccessResponseMapper {
+
+    @Mapping(source = "privilegedAccessIdentifier.privilegedAccessId", target = "privilegedAccessId")
+    PrivilegedAccessResponseModel entityToResponseModel(PrivilegedAccess privilegedAccess);
+
+    List<PrivilegedAccessResponseModel> entityToResponseModelList(List<PrivilegedAccess> privilegedAccesses);
 }
