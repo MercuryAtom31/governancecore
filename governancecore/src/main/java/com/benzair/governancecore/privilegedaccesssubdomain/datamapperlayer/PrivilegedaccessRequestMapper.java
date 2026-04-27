d16c2491feec154e2bc9bfa52ae63244cf89f410
Hichem <hichembenzair@gmail.com>
2026-04-27 00:08:15 -0400

    Privileged access request mapper.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datamapperlayer/PrivilegedaccessRequestMapper.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/datamapperlayer/PrivilegedaccessRequestMapper.java
@@ -1,5 +1,35 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.datamapperlayer;
 
-public class PrivilegedAccessRequestMapper {
-    
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccess;
+import com.benzair.governancecore.privilegedaccesssubdomain.datalayer.PrivilegedAccessRole;
+import com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer.PrivilegedAccessRequestModel;
+
+@Mapper(componentModel = "spring")
+public interface PrivilegedAccessRequestMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "privilegedAccessIdentifier", ignore = true)
+    @Mapping(target = "requesterUserId", ignore = true)
+    @Mapping(target = "requesterName", ignore = true)
+    @Mapping(target = "requesterCurrentRole", ignore = true)
+    @Mapping(target = "role", expression = "java(defaultRole())")
+    @Mapping(target = "requestedAt", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "grantedByUserId", ignore = true)
+    @Mapping(target = "grantedByName", ignore = true)
+    @Mapping(target = "approvalNote", ignore = true)
+    @Mapping(target = "grantedAt", ignore = true)
+    @Mapping(target = "refusedAt", ignore = true)
+    @Mapping(target = "refusalReason", ignore = true)
+    @Mapping(target = "expiresAt", ignore = true)
+    @Mapping(target = "revokedAt", ignore = true)
+    @Mapping(target = "revokeReason", ignore = true)
+    PrivilegedAccess requestModelToEntity(PrivilegedAccessRequestModel privilegedAccessRequestModel);
+
+    default PrivilegedAccessRole defaultRole() {
+        return PrivilegedAccessRole.ADMIN;
+    }
 }
