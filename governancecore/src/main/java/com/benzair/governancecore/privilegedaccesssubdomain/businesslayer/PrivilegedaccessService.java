a3ee83c6ddea7bf1ac0f01703c04ca2f452de7a4
Hichem <hichembenzair@gmail.com>
2026-04-26 23:26:11 -0400

    Created privileged access service interface methods.

--- governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/businesslayer/PrivilegedaccessService.java
+++ governancecore/src/main/java/com/benzair/governancecore/privilegedaccesssubdomain/businesslayer/PrivilegedaccessService.java
@@ -1,6 +1,7 @@
 package com.benzair.governancecore.privilegedaccesssubdomain.businesslayer;
 
 import com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer.PrivilegedAccessResponseModel;
+import com.benzair.governancecore.privilegedaccesssubdomain.presentationlayer.PrivilegedAccessRequestModel;
 
 import java.util.List;
 import java.util.UUID;
@@ -13,7 +14,17 @@ public interface PrivilegedAccessService {
 
     void deletePrivilegedAccessByPrivilegedAccessId(UUID privilegedAccessId);
 
-    PrivilegedAccessResponseModel addPrivilegedAccess(PrivilegedAccessResponseModel privilegedAccessRequestModel);
+    PrivilegedAccessResponseModel addPrivilegedAccess(PrivilegedAccessRequestModel privilegedAccessRequestModel);
 
-    PrivilegedAccessResponseModel updatePrivilegedAccess(PrivilegedAccessResponseModel privilegedAccessRequestModel, UUID privilegedAccessId);
+    PrivilegedAccessResponseModel updatePrivilegedAccess(PrivilegedAccessRequestModel privilegedAccessRequestModel, UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel grantPrivilegedAccess(UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel refusePrivilegedAccess(UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel revokePrivilegedAccess(UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel markPrivilegedAccessInUse(UUID privilegedAccessId);
+
+    PrivilegedAccessResponseModel expirePrivilegedAccess(UUID privilegedAccessId);
 }
